ddb8f363156f09872d82bff825dff1fe625a7675
Anderson  Kloss Maia <andersonklossmaia@MacBook-Pro-de-Anderson-2.local>
2017-11-02 12:05:17 -0200

    crypto added to bridging header and carthage added to project

---
+++ marvel-sample-app/marvel-sample-app/Network/NetworkSupport.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkSupport.swift
+//  marvel-sample-app
+//
+//  Created by Anderson  Kloss Maia on 02/11/17.
+//  Copyright © 2017 Anderson Kloss Maia. All rights reserved.
+//
+
+import Foundation
+
+class NetworkSupport {
+    
+    init(timeStamp: Date, publicKey: String, privateKey: String) {
+        
+    }
+}
---
+++ marvel-sample-app/marvel-sample-app/Resources/marvel-sample-app-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  marvel-sample-app-Bridging-Header.h
+//  marvel-sample-app
+//
+//  Created by Anderson  Kloss Maia on 02/11/17.
+//  Copyright © 2017 Anderson Kloss Maia. All rights reserved.
+//
+
+#ifndef marvel_sample_app_Bridging_Header_h
+#define marvel_sample_app_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* marvel_sample_app_Bridging_Header_h */
