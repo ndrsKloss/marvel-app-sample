96fe9fad2120a15de5d3bb9251bf4b23724b6953
Anderson  Kloss Maia <andersonklossmaia@MacBook-Pro-de-Anderson-2.local>
2017-11-02 11:15:12 -0200

    project created and gitignore added

---
+++ marvel-sample-app/marvel-sample-app/CharactersList/CharactersListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  marvel-sample-app
+//
+//  Created by Anderson  Kloss Maia on 02/11/17.
+//  Copyright © 2017 Anderson Kloss Maia. All rights reserved.
+//
+
+import UIKit
+
+class CharactersListViewController: UIViewController {
+}
+
